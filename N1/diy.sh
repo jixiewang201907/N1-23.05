08fed8dafe56e3273f655635d4ad035d52a25081
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-10-14 15:37:16 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.200/g' package/base-files/files/bin/config_generate
 
 # Remove packages
 rm -rf feeds/luci/applications/luci-app-passwall
@@ -9,3 +9,6 @@ rm -rf feeds/luci/applications/luci-app-passwall
 # Add packages
 git clone https://github.com/nantayo/My-Pkg package/my-pkg
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+git clone https://github.com/jixiewang201907/by-pass.git package/by-pass
+# Add luci-theme-opentomcat
+git clone https://github.com/jixiewang201907/luci-theme-opentomcat.git package/luci-theme-opentomcat
