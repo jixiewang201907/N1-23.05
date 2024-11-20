c6a106a14791242b523e86b10b7621fa118d016c
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-11-20 10:49:42 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -10,3 +10,11 @@ rm -rf feeds/luci/applications/luci-app-passwall
 git clone https://github.com/nantayo/My-Pkg package/my-pkg
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+# Add luci-app-bypass
+git clone https://github.com/jixiewang201907/by-pass.git package/by-pass
+# Add luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
+git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+# Add luci-app-passwall
+#git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+git clone -b main https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
