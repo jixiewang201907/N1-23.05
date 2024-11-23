e190e9ff3228c91b8079d8df798d65325af9463b
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2024-11-23 13:48:40 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -9,7 +9,7 @@ rm -rf feeds/luci/applications/luci-app-passwall
 wget -q -cP files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 wget -q -cP files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 # Add packages
-#git clone https://github.com/nantayo/My-Pkg package/my-pkg
+git clone https://github.com/jixiewang201907/My-Pkg package/my-pkg
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 # Add luci-app-bypass
@@ -18,5 +18,5 @@ git clone https://github.com/jixiewang201907/by-pass.git package/by-pass
 git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
 git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 # Add luci-app-passwall
-git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
-git clone -b main https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
+#git clone -b main https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+#git clone -b main https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
